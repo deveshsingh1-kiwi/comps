e064d635d60b638f4f1006c9bdafe7458d2342fc
deveshsingh1-kiwi <devesh.singh1@kiwitech.com>
2024-04-29 15:10:28 +0530

    Section 13: Making Navigation Reusable

--- src/pages/ButtonPage.js
+++ src/pages/ButtonPage.js
@@ -1,10 +1,8 @@
-import { GoBell, GoCloudDownload, GoDatabase } from 'react-icons/go';
-import Button from '../components/Button';
+import { GoBell, GoCloudDownload, GoDatabase } from "react-icons/go";
+import Button from "../components/Button";
 
 function ButtonPage() {
-  const handleClick = () => {
-    console.log('Clicked!');
-  };
+  const handleClick = () => {};
 
   return (
     <div>
