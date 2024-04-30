fcda327e5ae604cee47fe593649fc869489e678e
deveshsingh1-kiwi <devesh.singh1@kiwitech.com>
2024-04-30 17:25:24 +0530

    Section 16: Getting Clever with Data Sorting

--- src/pages/TablePage.js
+++ src/pages/TablePage.js
@@ -1,5 +1,5 @@
-import { render } from "@testing-library/react";
-import Table from "../components/Table";
+// import Table from '../components/Table';
+import SortableTable from "../components/SortableTable";
 
 function TablePage() {
   const data = [
@@ -10,12 +10,20 @@ function TablePage() {
   ];
 
   const config = [
-    { label: "Name", render: (fruit) => fruit.name },
+    {
+      label: "Name",
+      render: (fruit) => fruit.name,
+      sortValue: (fruit) => fruit.name,
+    },
     {
       label: "Color",
-      render: (fruit) => <div className={`p-3 m-2 ${fruit.color}`}></div>,
+      render: (fruit) => <div className={`p-3 m-2 ${fruit.color}`} />,
     },
-    { label: "Score", render: (fruit) => fruit.score },
+    {
+      label: "Score",
+      render: (fruit) => fruit.score,
+      sortValue: (fruit) => fruit.score,
+    },
   ];
 
   const keyFn = (fruit) => {
@@ -24,7 +32,7 @@ function TablePage() {
 
   return (
     <div>
-      <Table data={data} config={config} keyFn={keyFn} />
+      <SortableTable data={data} config={config} keyFn={keyFn} />
     </div>
   );
 }
